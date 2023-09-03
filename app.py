3198d81b529e943a5b7d75e33fbb42cd28f2145c
david-barrei <davbarreiro4@gmail.com>
2023-09-02 23:38:25 -0500

    Funciones para manejar base de datos

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import bd.base_datos as sqlbd
 base_datos = sqlbd.BaseDatos(**sqlbd.acceso_bd)
 
 
-consulta_1 = base_datos.consulta("SHOW DATABASES")
 
-for bd in consulta_1: #recorrer la base de datos
-    print(bd)
+
+base_datos.crear_bd("pruebas1")
+
+
--- bd/base_datos.py
+++ bd/base_datos.py
@@ -2,14 +2,47 @@ import mysql.connector
 
 acceso_bd = {"host": "localhost",
              "user": "root",
-             "password": "root",
-             "databases" : "app"}
+             "password": ""
+             }
 
 class BaseDatos:
     def __init__(self, **kwargs):
         self.conector = mysql.connector.connect(**kwargs)
+        self.cursor = self.conector.cursor()
+    
+    #Decorador para el reporte de base de datos
+    def reporte_bd(funcion_parametro):
+        def interno(self, nombre_bd):
+            funcion_parametro(self, nombre_bd)
+            print("Estas son las bases de datos creadas\n")
+            BaseDatos.mostrar_bd(self)
+        return interno
 
     def consulta(self, sql):
-        cursor = self.conector.cursor()
-        cursor.execute(sql)
-        return cursor
+        self.cursor.execute(sql)
+        return self.cursor
+    
+    #muestra la base de datos del servidor
+    def mostrar_bd(self):
+        self.cursor.execute("SHOW DATABASES")
+        for bd in self.cursor:
+            print(bd)
+
+    #eliminar una base de datos
+    @reporte_bd
+    def eliminar_bd(self, nombre_bd):
+        try:
+            self.cursor.execute(f"DROP DATABASE {nombre_bd}")
+            print(f"Se elimino correctamente {nombre_bd}")
+        except:
+            print(f"Base de datos {nombre_bd} no encontrada\n")
+           
+    @reporte_bd
+    def crear_bd(self, nombre_bd):
+        try:
+            self.cursor.execute(f"CREATE DATABASE IF NOT EXISTS {nombre_bd}")
+            print(f" Se creo la base de datos {nombre_bd}")
+        except:
+            print(f"Ocurrio un error ")
+
+
